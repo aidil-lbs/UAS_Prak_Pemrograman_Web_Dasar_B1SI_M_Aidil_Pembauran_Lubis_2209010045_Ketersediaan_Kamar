eade2e3a7be36ae1b01063af56602b8423a0e2dd
putriputss <r.rfan1180@gmail.com>
2024-06-23 22:42:35 +0700

    Analisi Table

---
+++ UASPWD_Aidil/database/migrations/2024_06_23_153328_create_bookings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pasien_id')->constrained('data_pasien')->onDelete('cascade');
+            $table->foreignId('kamar_id')->constrained('kamar')->onDelete('cascade');
+            $table->dateTime('tanggal_checkin');
+            $table->dateTime('tanggal_checkout')->nullable();
+            $table->enum('status', ['pending', 'aktif', 'selesai'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
